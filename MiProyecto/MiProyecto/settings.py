8ea6bb937d454dbeeadcdfb4500840f10ef9e215
PameGatica <pam.gatica@profesor.duoc.cl>
2021-06-15 16:14:35 -0400

    conexión a oracle y superuser

--- MiProyecto/MiProyecto/settings.py
+++ MiProyecto/MiProyecto/settings.py
@@ -80,8 +80,15 @@ WSGI_APPLICATION = 'MiProyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1:1521/xe',
+        'USER': 'django006',
+        'PASSWORD':'django006',
+        'TEST':{
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        }
     }
 }
 
