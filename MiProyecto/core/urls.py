bb408967cb1fc3d648130b6daaa785212897fd4d
PameGatica <pam.gatica@profesor.duoc.cl>
2021-06-22 16:27:29 -0400

    CRUD II : Update y Delete

--- MiProyecto/core/urls.py
+++ MiProyecto/core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import index, contacto, listarVehiculos, agregarVehiculo
+from .views import index, contacto, listarVehiculos, agregarVehiculo, editarVehiculo,eliminarVehiculo
 
 urlpatterns = [
     path('',index, name="index"),
     path('contacto/',contacto, name="contacto"),
     path('vehiculo/listar',listarVehiculos, name="listarVehiculos"),
     path('vehiculo/agregar',agregarVehiculo,name="agregarVehiculo"),
+    path('vehiculo/editar/<id>',editarVehiculo,name="editarVehiculo"),
+    path('vehiculo/eliminar/<id>',eliminarVehiculo,name="eliminarVehiculo")
 
 ]
