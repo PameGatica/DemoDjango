1506bde9db84addc0ef716606b76efc04f378165
PameGatica <pam.gatica@profesor.duoc.cl>
2021-07-06 16:23:18 -0400

    DRF + Token Auth

--- MiProyecto/rest/urls.py
+++ MiProyecto/rest/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from rest.views import detalle_vehiculo, lista_vehiculos
+from .viewslogin import login
 
 
 urlpatterns = [
     path('vehiculos',lista_vehiculos,name="lista_vehiculos"),
-    path('vehiculos/<id>',detalle_vehiculo,name="detalle_vehiculo")
+    path('vehiculos/<id>',detalle_vehiculo,name="detalle_vehiculo"),
+    path('login', login, name='login')
 ]
--- MiProyecto/rest/views.py
+++ MiProyecto/rest/views.py
@@ -3,15 +3,16 @@ from rest.serializers import VehiculoSerializer
 from django.shortcuts import render
 from core.models import Vehiculo
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.parsers import JSONParser
 from rest_framework import status
+from rest_framework. permissions import IsAuthenticated
 
 #GET : Listar todos los vehiculos
 #POST: Nuevo vehículo
 @csrf_exempt
 @api_view(['GET','POST'])
-
+@permission_classes((IsAuthenticated,))
 def lista_vehiculos(request):
     if request.method == 'GET':
         vehiculos = Vehiculo.objects.all()
@@ -27,6 +28,7 @@ def lista_vehiculos(request):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
 
 @api_view(['GET','PUT','DELETE'])
+@permission_classes((IsAuthenticated,))
 def detalle_vehiculo(request, id):
     try:
         vehiculo = Vehiculo.objects.get(patente=id)
